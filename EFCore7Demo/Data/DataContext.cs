3c3380bc63bfec7bd45c4c44123514fec72dddab
Fatih <mfatihinan@gmail.com>
2023-02-20 15:05:38 -0500

    First entity creation and EF setup

---
+++ EFCore7Demo/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore7Demo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EFCore7Demo/User.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCore7Demo
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        public string Username { get; set; } = string.Empty;
+    }
+}
